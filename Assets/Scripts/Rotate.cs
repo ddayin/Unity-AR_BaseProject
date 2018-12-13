129e8707edb501f2d0ada1b9e96a74124ce2941d
ddayin <ddayin@hotmail.com>
2018-12-13 21:03:03 +0900

    -

---
+++ Assets/Scripts/Rotate.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotate : MonoBehaviour {
+
+    private Transform m_Transform;
+    public float m_Speed = 10f;
+
+    // Use this for initialization
+    void Start()
+    {
+        m_Transform = transform;
+	}
+	
+	// Update is called once per frame
+	void Update()
+    {
+        m_Transform.Rotate(0f, Time.deltaTime * m_Speed, 0f, Space.Self);
+	}
+}
